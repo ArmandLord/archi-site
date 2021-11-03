87e3398a1a5d3bf59601c0293374ba7f49f1a55f
Gabriel Sanchez <54195784+GabSanWebDev@users.noreply.github.com>
2021-11-03 14:14:05 -0300

    sincronizacion

--- src/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as LayoutComponent } from './LayoutComponent/LayoutComponent' 
-export { default as CarouselComponent } from './CarouselComponent/CarouselComponent' 
-export { default as CardComponent } from './CardComponent/CardComponent' 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,3 @@
 export { default as Home } from './Home/Home' 
 export { default as NotFound } from './NotFound/NotFound' 
-
-import React from 'react'
-
-function NotFound() {
-    return (
-        <div>
-            404 not found
-        </div>
-    )
-}
-
-export default NotFound
+export {default as About } from './About/About' 
