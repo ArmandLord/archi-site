baad14affac07887024b852f04ff63946d5890bf
Armando Perez <armandlord98@gmail.com>
2021-11-03 11:13:12 -0600

    Igual

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,1 @@
-export { default as LayoutComponent } from './LayoutComponent/LayoutComponent' 
-export { default as CarouselComponent } from './CarouselComponent/CarouselComponent' 
-export { default as CardComponent } from './CardComponent/CardComponent' 
+export { default as CarouselC } from './CarouselC/CarouselC'
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,4 @@
 export { default as Home } from './Home/Home' 
 export { default as NotFound } from './NotFound/NotFound' 
 
-import React from 'react'
 
-function NotFound() {
-    return (
-        <div>
-            404 not found
-        </div>
-    )
-}
-
-export default NotFound
