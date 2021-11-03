0b34a7dff70ec71e2ce76b7124f90ff127f4c8e5
Armando Perez <armandlord98@gmail.com>
2021-11-03 11:44:35 -0600

    global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import GlobalStyles from './GlobalStyles/GlobalStyles'
 import { Home, NotFound, About } from './pages'
-
+import { Navbar, Footer } from './components'
 
 
 function App() {
   return (
     <Router>
+      <GlobalStyles/>
+      <Navbar/>
         <Switch>
           <Route exact path='/' component={Home}/> 
           <Route exact path='/about' component={About}/> 
           <Route exact path='*' component={NotFound}/> 
         </Switch>
+      <Footer/>
     </Router>
   );
 }
